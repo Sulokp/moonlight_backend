e17dfb1aa5dc6bbd4b7e7271d0d21cc6d3c5ba32
Sulok Pandey <np03cs4a220038@heraldcollege.edu.np>
2026-05-08 13:18:57 +0545

    middleware for admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { loginAdmin } = require("../controllers/adminController");
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/login", loginAdmin);
+router.get("/profile", authMiddleware, (req, res) => {
+    res.json({
+        message: "Protected admin data",
+        admin: req.admin
+    });
+});
 
 module.exports = router;
