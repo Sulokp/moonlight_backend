45eacfc4af97ac7040a21581ef0a97ff754bde7e
Sulok Pandey <np03cs4a220038@heraldcollege.edu.np>
2026-05-09 11:56:33 +0545

    cors added

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 require("dotenv").config();
 
 require("./config/db");
-
+app.use(cors());
 app.use(express.json());
+
 
 // Routes
 app.use("/api/admin", require("./routes/adminRoutes"));
